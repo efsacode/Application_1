e1fd461542b0183a41bfdc8ca5202a29793b727f
efsa <113553752+efsacode@users.noreply.github.com>
2023-11-21 22:58:42 +0300

    DB connection

---
+++ Models/EmployeeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Application1.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Veritabanı bağlantı dizesini buraya ekleyin
+            optionsBuilder.UseSqlServer("Server=localhost;Database=EmployeeDB;Trusted_Connection=True;");
+        }
+    }
+}
